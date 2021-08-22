07bb5f34e8620fffc317ed0889908005f799ba20
duanjunyan <duanjunyan@autohome.com.cn>
2021-08-22 19:00:40 +0800

    postcss config

---
+++ postcss.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+    syntax: 'postcss-scss',
+    plugins: [
+        require("autoprefixer")({
+        overrideBrowserslist: [
+                "Android 4.1",
+                "iOS 7.1",
+                "Chrome > 31",
+                "ff > 31",
+                "ie >= 8",
+                "last 10 versions" // 所有主流浏览器最近10版本用
+            ],
+            grid: true
+        }),
+        require("postcss-nested")
+    ]
+}
