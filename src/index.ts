fc09c4c47f4c487e90c3a2978d431532f068fb44
hsq <13641276693@163.com>
2021-09-05 16:00:44 +0800

    add:switch

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,20 @@
 import Cell from '../packages/cell';
 import Toast from '../packages/toast';
 import Button from '../packages/button';
+import Switch from '../packages/switch';
 import {App} from 'vue';
 
 const install = function (app: App, options: Object) {
     app.component(Cell.name, Cell);
     app.component(Button.name, Button);
     app.component(Toast.name, Toast);
+    app.component(Switch.name, Switch);
 }
 
 export {
     install,
     Cell,
     Toast,
-    Button
+    Button,
+    Switch
 }
